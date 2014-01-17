b0832692cc791e72ef1cf42f9119b8316c1a9cd0
bugobliterator <siddharthbharatpurohit@gmail.com>
2014-01-17 14:56:59 +0530

    cleanup repository of useless library files and demos

--- FreeRTOS/Demo/AVR_ATMega323_WinAVR/serial/serial.d
+++
@@ -1,7 +0,0 @@
-serial.o serial.d : serial/serial.c ../../Source/include/FreeRTOS.h \
- ../../Source/include/projdefs.h FreeRTOSConfig.h \
- ../../Source/include/portable.h \
- ../../Source/include/../portable/GCC/ATMega323/portmacro.h \
- ../../Source/include/mpu_wrappers.h ../../Source/include/queue.h \
- ../../Source/include/task.h ../../Source/include/list.h \
- ../Common/include/serial.h
---
+++ UnoBlink/Queue.c
@@ -0,0 +1,107 @@
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/* Scheduler include files. */
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <semphr.h>
+#include <ringBuffer.h>
+/* serial interface include file. */
+#include <lib_serial.h>
+/* ADC interface include file. */
+#include <digitalAnalog.h>
+xQueueHandle xQueue;
+xComPortHandle xSerialPort1;
+xComPortHandle xSerialPort2;
+
+void vTask1( void *pvParameters );
+void vTask2( void *pvParameters );
+void vTask3( void *pvParameters );
+
+void vApplicationIdleHook( void );
+void vTask1( void *pvParameters )
+{
+	const char *pcTaskName = "Task 1 is running\n";
+	char charvar[10];
+	volatile unsigned long ul;
+	char *ReceivedValue;
+	portBASE_TYPE xStatus;
+	const portTickType xTicksToWait = 100;
+	xSerialPort = xSerialPortInitMinimal( USART0, 115200, 10, 10); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
+	
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	while(1)
+	{	
+		xStatus = xQueueReceive( xQueue, &ReceivedValue, xTicksToWait );
+		if(!analogIsConverting()) {
+				xSerialxPrintf(&xSerialPort,ReceivedValue);
+		}
+	}
+}
+void vTask2( void *pvParameters )
+{
+	const char *pcTaskName = "Task 2 is running\n";
+	volatile unsigned long ul,i;
+	DDRB=0xFF;
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	while(1)
+	{	
+		PORTB=0xFF;
+		for(i=0;i<10000;i++);
+		PORTB=0x00;
+		for(i=0;i<10000;i++);
+	}
+}
+
+void vTask3( void *pvParameters )
+{
+	portBASE_TYPE xStatus;
+	const portTickType xTicksToWait = 100;
+	const char *st = "Sid";
+	for( ;; )
+	{
+		xStatus = xQueueSendToBack( xQueue, pvParameters, xTicksToWait );
+		taskYIELD();
+	}
+}
+
+void vApplicationIdleHook( void )
+{
+	vCoRoutineSchedule();
+}
+
+
+int main( void )
+{
+	/* Create one of the two tasks. Note that a real application should check
+	the return value of the xTaskCreate() call to ensure the task was created
+	successfully. */
+	
+		
+	//avrSerialxPrint_P(&xSerialPort, PSTR("\r\nStart Processes-->\n")); // Ok, so we're alive...
+	xQueue = xQueueCreate( 10, sizeof( char ) );
+	xTaskCreate(vTask1, /* Pointer to the function that implements the task. */
+		"Task 1",/* Text name for the task. This is to facilitate debugging only. */
+		240,/* Stack depth in words. */
+		NULL,/* We are not using the task parameter. */
+		1,/* This task will run at priority 1. */
+		NULL ); /* We are not going to use the task handle. */
+	/* Create the other task in exactly the same way and at the same priority. */
+	xTaskCreate( vTask2, "Task 2", 240, NULL, 1, NULL );
+
+	xTaskCreate( vTask3, "Task 2", 240, NULL, 1, NULL );
+	
+	/* Start the scheduler so the tasks start executing. */
+	vTaskStartScheduler();
+	/* If all is well then main() will never reach here as the scheduler will
+	now be running the tasks. If main() does reach here then it is likely that
+	there was insufficient heap memory available for the idle task to be created.
+	CHAPTER 5 provides more information on memory management. */
+	return 0;
+}	
